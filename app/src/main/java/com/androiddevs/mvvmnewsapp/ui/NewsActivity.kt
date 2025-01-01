ea324fcf60e241a036bdc77a64fa512b0b832c7b
ahmedmmostaafaa@gmail.com <medooortega.mx@gmail.com>
2025-01-01 19:50:36 +0200

    Added basic fragments and NewsActivity ( The Host )

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -1,12 +1,24 @@
-package com.androiddevs.mvvmnewsapp
+package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.Repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.db.articleDatabase
+import kotlinx.android.synthetic.main.activity_news.bottomNavigationView
+import kotlinx.android.synthetic.main.activity_news.newsNavHostFragment
 
 class NewsActivity : AppCompatActivity() {
-
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+        val repository =NewsRepository(articleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository = repository)
+        viewModel = ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
